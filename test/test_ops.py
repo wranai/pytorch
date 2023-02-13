31016164fccc030e0f6c868a79c15d6153ea19ad
Edward Z. Yang <ezyang@meta.com>
2023-02-13 08:33:41 -0800

    Get boolean masking to work with unbacked SymInts
    Signed-off-by: Edward Z. Yang <ezyangmeta.com>
    
    ghstack-source-id: ff8dbade09a450d3e437497f3cf806fb8618d87a
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/94523

--- test/test_ops.py
+++ test/test_ops.py
@@ -461,7 +461,14 @@ class TestCommon(TestCase):
         # In this test, primTorch refs call into the refs namespace
         # For example, a ref with torch.foo in it will calls refs.foo instead
         # Direct calls to refs and prims are not affected
-        self._ref_test_helper(lambda: TorchRefsMode(strict=True), device, dtype, op)
+        # Note [PrimTorch cannot do view consistency correctly]
+        # NB: skip_view_consistency as PrimTorch does not have enough tools
+        # to correctly reflect view-ness of tensors while simultaneously
+        # having good guard-free implementations (main example is allocate
+        # contiguous and permute; this needs to produce a non-view tensor
+        # but there is no way to express this; empty_strided is incorrect
+        # as it results in too many guards.)
+        self._ref_test_helper(lambda: TorchRefsMode(strict=True), device, dtype, op, skip_view_consistency=True)
 
     # Tests that experimental Python References perform the same computation
     # as the operators they reference, when operator calls in the torch
@@ -526,7 +533,8 @@ class TestCommon(TestCase):
             skip_bfloat=("nvfuser" in executor),  # nvfuser doesn't support bfloat tensors for pre-11 cuda TK
             # # nvfuser doesn't support view consistency
             # https://github.com/pytorch/pytorch/issues/84863
-            skip_view_consistency=("nvfuser" in executor),
+            # See also Note [PrimTorch cannot do view consistency correctly]
+            skip_view_consistency=True,
         )
 
     @skipMeta
